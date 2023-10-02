f13189104b0af33fa3ef35512f3a8d800f1fa93a
TheBv <bv939@web.de>
2023-10-02 19:35:14 +0200

    Initial commit part-4

--- fetch.ts
+++ fetch.ts
@@ -1,7 +1,9 @@
 const IMAGE_URL = "https://images.ygoprodeck.com/images/cards"
+const IMAGE_URL_FIELD = "https://images.ygoprodeck.com/images/cards_cropped"
 const BATCH_SIZE = 19 // ygoprodeck has a rate limit of 20 requests per second
 
 const DOWNLOAD_DIR = "./pics"
+const DOWNLOAD_DIR_FIELD = "./pics/field"
 
 interface ResponseData {
     id: number,
@@ -28,12 +30,14 @@ let queue = []
 
 let counter = 0
 
+
+// Fetch cards
 for (const entry of data) {
     const downloadPath = `${DOWNLOAD_DIR}/${entry.id}.jpg`
     const file = Bun.file(downloadPath)
     // Let's not download the images we already have downloaded
     if (!(await file.exists())) {
-        queue.push(fetchCard(`${IMAGE_URL}/${entry.id}.jpg`, downloadPath))
+        queue.push(fetchArt(`${IMAGE_URL}/${entry.id}.jpg`, downloadPath))
     }
     if (queue.length >= BATCH_SIZE) {
         await Promise.allSettled(queue)
@@ -48,8 +52,37 @@ for (const entry of data) {
         console.log("Progress:", ((counter / data.length) * 100).toFixed(4), "%")
 }
 
+// Fetch fields
+counter = 0
 
-async function fetchCard(url: string, downloadPath: string) {
+const params = new URLSearchParams()
+params.set("type", "Spell Card")
+params.set("race", "field")
+const fieldResponse = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?${params.toString()}`)
+const fieldJson = await fieldResponse.json() as Response
+const fieldData = fieldJson.data
+
+for (const entry of fieldData) {
+    const downloadPath = `${DOWNLOAD_DIR_FIELD}/${entry.id}.jpg`
+    const file = Bun.file(downloadPath)
+    // Let's not download the images we already have downloaded
+    if (!(await file.exists())) {
+        queue.push(fetchArt(`${IMAGE_URL_FIELD}/${entry.id}.jpg`, downloadPath))
+    }
+    if (queue.length >= BATCH_SIZE) {
+        await Promise.allSettled(queue)
+        // Probably log some stuff here
+        queue = []
+        // Wait for one second
+        await Bun.sleep(1000)
+    }
+
+    counter++
+    if (counter % 200 == 0)
+        console.log("Progress:", ((counter / data.length) * 100).toFixed(4), "%")
+}
+
+async function fetchArt(url: string, downloadPath: string) {
     try {
         const response = await fetch(url)
         if (response.ok) {
